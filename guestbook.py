b00623fc7892cb069db91b688d56b55e1f25b92f
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 19:10:40 +0700

    test

--- guestbook.py
+++ guestbook.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column,Integer,String,Date
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:OEIknz76159@node1438-thitimon.app.ruk-com.cloud:11023/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:OEIknz76159@10.100.2.53:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 class Comments(db.Model):
